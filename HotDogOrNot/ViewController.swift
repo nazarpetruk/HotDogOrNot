675acb5fdc74686d524ba9d76a22d2d971fcf287
nazar petruk <nazarpetruk15@gmail.com>
2019-08-27 16:27:40 +0200

    Initial Commit

---
+++ HotDogOrNot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotDogOrNot
+//
+//  Created by Nazar Petruk on 27/08/2019.
+//  Copyright © 2019 Nazar Petruk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
